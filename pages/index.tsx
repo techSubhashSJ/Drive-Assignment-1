0744865cee0ca3118334f05acaa0ce9536086573
Subhash Jadhav <subhash.jadhav@bluepineapple.io>
2022-10-31 17:08:24 +0530

    put refresh button in the page container

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,7 +55,7 @@ const Home: NextPage<{ data: myData }> = ({ data }) => {
 
   return (
     <>
-      <div className="container  p-4">
+      <div className="container px-16 p-4  mt-7 max-w-full xl:max-w-7xl xl:mx-auto">
         <button
           className="bg-blue-700 text-white px-4 py-2 rounded-full baseline hover:bg-blue-400"
           onClick={() => router.reload()}
@@ -64,7 +64,7 @@ const Home: NextPage<{ data: myData }> = ({ data }) => {
         </button>
       </div>
 
-      <div className="container px-12 p-4  mt-7 max-w-full xl:max-w-7xl xl:mx-auto">
+      <div className="container px-16 p-4  mt-7 max-w-full xl:max-w-7xl xl:mx-auto">
         {(error || warning) && (
           <div
             className={
@@ -120,12 +120,12 @@ const Home: NextPage<{ data: myData }> = ({ data }) => {
       {loading && makes?.length === 0 ? (
         <Spinner />
       ) : !loading && error === null && url !== null && makes.length === 0 ? (
-        <div className="container p-4 px-12 mt-7 max-w-full xl:max-w-7xl xl:mx-auto">
+        <div className="container p-4 px-16 mt-7 max-w-full xl:max-w-7xl xl:mx-auto">
           <h1 className="text-2xl font-bold">No Result Found</h1>
         </div>
       ) : (
         <>
-          <div className="container px-12 p-4  mt-7 max-w-full xl:max-w-7xl xl:mx-auto">
+          <div className="container px-16 p-4  mt-7 max-w-full xl:max-w-7xl xl:mx-auto">
             <h1 className="text-2xl font-extrabold">
               {makes[0] ? `${makes[0]?.Make_Name} Models: ` : ""}
             </h1>
